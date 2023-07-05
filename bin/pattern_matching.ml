1fd0616774670c8cb43437c0d36d0417162c2c8a
CGQAQ <m.jason.liu@outlook.com>
2023-07-05 06:17:53 +0000

    pattern matching

--- bin/pattern_matching.ml
+++ bin/pattern_matching.ml
@@ -11,14 +11,22 @@ let plus = function
 | THREE, ONE -> 4
 | THREE, TWO -> 5
 | THREE, THREE -> 6
-
 (* Output 4 *)
-let () = print_int (plus (ONE, THREE))
+let () = print_int (plus (ONE, THREE)); print_newline ()
 (* Output 3 *)
-let () = print_int (plus (ONE, TWO))
+let () = print_int (plus (ONE, TWO)); print_newline ()
 
 
 let negate = fun x: int -> - x
 (* Output 1 *)
-let () = print_int (negate (-1))
+let () = print_int (negate (-1)); print_newline ()
+(* Output -1 *)
+let () = print_int (negate 1); print_newline ()
 
+
+let rec fact = fun x: int -> 
+  match x with
+  | 0 -> 1
+  | x -> x * fact (x - 1)
+(* Output 120 *)
+let () = print_int (fact 5); print_newline ()
